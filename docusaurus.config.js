88dbdcb799bb3fea8fe3d762e39ac7aeabc5f785
mnida <mikaelnida@gmail.com>
2022-09-18 17:44:48 -0700

    remove edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,9 +34,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/uselotus/lotus-docs",
           remarkPlugins: [
             [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
           ],
